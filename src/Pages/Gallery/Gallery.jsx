d49dc00e080e800db809995aa0812e0b4d4c2904
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-11-02 11:46:40 +0600

    gallery route complete

---
+++ src/Pages/Gallery/Gallery.jsx
@@ -0,0 +1,33 @@
+import gallery1 from '../../assets/Gallery/gallery1.png'
+import gallery2 from '../../assets/Gallery/gallery2.png'
+import gallery3 from '../../assets/Gallery/gallery3.png'
+import gallery4 from '../../assets/Gallery/gallery4.png'
+import gallery5 from '../../assets/Gallery/gallery5.png'
+import gallery6 from '../../assets/Gallery/gallery6.png'
+import gallery7 from '../../assets/Gallery/gallery7.png'
+import gallery8 from '../../assets/Gallery/gallery8.png'
+import gallery9 from '../../assets/Gallery/gallery9.png'
+import gallery10 from '../../assets/Gallery/gallery10.png'
+import gallery11 from '../../assets/Gallery/gallery11.png'
+import gallery12 from '../../assets/Gallery/gallery12.png'
+import gallery13 from '../../assets/Gallery/gallery13.png'
+import gallery14 from '../../assets/Gallery/gallery14.png'
+import gallery15 from '../../assets/Gallery/gallery15.png'
+
+const Gallery = () => {
+
+    const gallery = [gallery1, gallery3, gallery2, gallery4, gallery5, gallery9, gallery6, gallery7, gallery8, gallery10, gallery11, gallery12, gallery13, gallery14, gallery15]
+
+    return (
+        <div className="pt-64">
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-7 my-container'>
+                {
+                    gallery.map(img => <img key={img} src={img}></img>)
+                }
+            </div>
+            <button className="btn-pri block mx-auto my-20">Load More</button>
+        </div>
+    );
+};
+
+export default Gallery;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Homepage/Home";
 import Committee from "../Pages/Committee/Committee";
 import AllEvent from "../Pages/AllEvent/AllEvent";
+import Gallery from "../Pages/Gallery/Gallery";
 
 export const router = createBrowserRouter([
     {
@@ -18,8 +19,12 @@ export const router = createBrowserRouter([
                 element: <Committee></Committee>
             },
             {
-                path: 'all-event',
+                path: '/all-event',
                 element: <AllEvent></AllEvent>
+            },
+            {
+                path: '/gallery',
+                element: <Gallery></Gallery>
             }
         ]
     }
